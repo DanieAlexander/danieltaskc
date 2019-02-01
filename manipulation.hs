1358eaa7d61fc9ed48456b01c0a454c957dc116d
DanieAlexander <daniel59953@gmail.com>
2019-01-31 23:49:54 -0500

    made changes

--- manipulation.hs
+++ manipulation.hs
@@ -1,6 +1,13 @@
 manipulation.hs
 
+import Data.List
 stringManip :: String -> Integer -> String
 stringManip str x
     | even x = str
 | odd x = reverse str
+
+stringBoustroph :: [String] -> [String]
+stringBoustroph str = [if odd(genericLength s) then reverse s else s | s <- str]
+
+addNums :: Int -> Int -> Int
+addNums a b = a + b
